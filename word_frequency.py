638c1b00e004f147c39bf6e1c6f2682e544d180e
KelC702 <kellamont2@gmail.com>
2019-10-08 09:25:55 -0400

    still working

--- word_frequency.py
+++ word_frequency.py
@@ -7,7 +7,32 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    
+    this_file = this_file.split()
+
+with open(file) as the_file
+ this_file = this_file.read
+
+def clean_text(text)
+    text = text.lower()
+    text_to_keep = ""
+    for char in text:
+        if char isalpha():
+            text_to_keep +=char
+            
+      clean_words = []
+      print(text_to _keep)
+
+       def clean_text(text):
+            all_letters = "abcdefghijklmnopqrstuvwxyz"
+            keep_text = ""
+            for char in text:
+                if char in all_letters:
+                    keep_text += char
+            return keep_text
+
+        clean_words = []
+
 
 
 if __name__ == "__main__":
@@ -25,3 +50,6 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+        seneca_falls.read()
+        
+
